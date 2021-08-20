6f470961222295bb6b5b4f97a8948ce4556ddff8
Martin Galan <martin.galan@snappler.com>
2021-08-20 18:07:20 -0300

    Modifies redmine url

--- src/configs.ts
+++ src/configs.ts
@@ -1,7 +1,7 @@
-export const WORK_SHEET_NAME = "TestTemplate";
+export const WORK_SHEET_NAME = "Agosto";
 export const WORK_SHEET_RANGE = "B3:J1000";
 export const WORK_SHEET_LOAD_CELL = "J3";
 
-export const REDMINE_URL = "https://test-redmine.snappler-app.com";
+export const REDMINE_URL = "https://redmine.snappler.com/";
 
 export const spreadsheetId = "101XvczXF6UW3wc2rpnsF7DsfxeTlQ9KUyDQ30yFzoeg";
